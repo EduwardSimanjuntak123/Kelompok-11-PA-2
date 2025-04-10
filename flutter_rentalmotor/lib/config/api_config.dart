a991c03dbab3b12b5ab825c2e1ae6e1e7a265199
raysimarmata23@gmail.com <149449737+RaySimarmata@users.noreply.github.com>
2025-04-10 10:47:34 +0700

    logo and naming apk

--- flutter_rentalmotor/lib/config/api_config.dart
+++ flutter_rentalmotor/lib/config/api_config.dart
@@ -2,9 +2,5 @@
 
 class ApiConfig {
   // Base URL API
-<<<<<<< HEAD
-  static const String baseUrl = "http://172.27.65.197:8081";
-=======
-  static const String baseUrl = "http://192.168.132.159:8080";
->>>>>>> 9efd7c0c2d18be0370051b7749a8c28f93d5586e
+  static const String baseUrl = "http://192.168.25.201:8081";
 }
