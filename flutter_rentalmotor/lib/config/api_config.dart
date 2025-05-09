07da7f9cae5d7a6efd268dd198344e28d6339c83
unknown <boasrayhanturnip@gmail.com>
2025-05-09 09:32:32 +0700

    pengujian

--- flutter_rentalmotor/lib/config/api_config.dart
+++ flutter_rentalmotor/lib/config/api_config.dart
@@ -1,7 +1,7 @@
 // lib/config/api_config.dart
 class ApiConfig {
   // Base URL API
-  static const String baseUrl = "http://192.168.34.159:8080";
+  static const String baseUrl = "http://192.168.214.159:8080";
 
-  static const String wsUrl = "ws://192.168.34.159:8080";
+  static const String wsUrl = "ws://192.168.214.159:8080";
 }
