034cbc206cde54d709f850621d725f7bb418d981
raysimarmata23@gmail.com <149449737+RaySimarmata@users.noreply.github.com>
2025-05-27 07:55:36 +0700

    addasd

--- flutter_rentalmotor/lib/config/api_config.dart
+++ flutter_rentalmotor/lib/config/api_config.dart
@@ -1,9 +1,7 @@
 // lib/config/api_config.dart
 class ApiConfig {
   // Base URL API
-  static const String baseUrl =
-      "https://backend-rental-golang-production.up.railway.app";
+  static const String baseUrl = "http://192.168.50.97:8081";
 
-  static const String wsUrl =
-      "wss://backend-rental-golang-production.up.railway.app";
+  static const String wsUrl = "wss://192.168.50.97:8081";
 }
